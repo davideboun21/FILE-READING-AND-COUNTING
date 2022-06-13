35c4e9e7debc00d804739ee85c6118f2e5c29900
davideboun21 <60818214+davideboun21@users.noreply.github.com>
2022-06-13 13:43:21 +0100

    added story.txt and main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+def read_file_content(filename):
+    with open(filename) as f:
+        contents = f.read()
+        return contents
+
+
+def count_words(words):
+    words = read_file_content('story.txt').split()
+    counts = dict()
+
+    for word in words:
+        if word in counts:
+            counts[word] += 1
+        else:
+            counts[word] = 1
+
+    return counts
+
+
+print(count_words('story.txt'))
